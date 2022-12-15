8cc3aa456cab89351386509278927977e81687a1
richardvoma <richardvoma1@gmail.com>
2022-12-14 23:18:58 -0500

    defining input

---
+++ add_variable.py
@@ -0,0 +1,21 @@
+# store input numbers from the user
+a = input("enter the first number: ")
+b = input("enter the second number: ")
+# add the number
+sum = float(a)+float(b)
+# print results
+print("the sum of {0} and {1} is {2}".format(a, b, sum))
+v = input("enter the first number: ")
+f = input("enter the second number: ")
+# mult the number
+mult = float(v)*float(f)
+# print results
+print("the mult of {0} and {1} is {2}".format(v, f, mult))
+# sub the number
+sub = float(v)-float(f)
+# print results
+print("the sub of {0} and {1} is {2}".format(v, f, sub))
+# div the numbers
+div = float(v)/float(f)
+# print the results
+print("the div of {0} and {1} is {2}".format(v, f, div))
